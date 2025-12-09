3aacf81b63bf7b8843b295260d8fc4b02416b7b9
Ayesha Qamar <f219646@cfd.nu.edu.pk>
2025-12-09 19:01:28 +0500

    Update user_signup

--- lib/view/authScreens/userView/user_signup.dart
+++ lib/view/authScreens/userView/user_signup.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+/*import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';*/
 
 class UserRegisterPage extends StatefulWidget {
   const UserRegisterPage({super.key});
@@ -163,3 +163,4 @@ class _UserRegisterPageState extends State<UserRegisterPage> {
     );
   }
 }
+
